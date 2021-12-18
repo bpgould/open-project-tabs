69ca3043a333de52da9da2aaf01bbceda9f38a2a
Bennett Gould <bgould@Bennetts-MacBook-Pro.local>
2021-12-17 21:25:41 -0500

    preparing to publish

---
+++ openTabs.js
@@ -0,0 +1,48 @@
+const open = require("open");
+const readline = require("readline");
+const fs = require("fs");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const tabsArray = [];
+const argv = yargs(hideBin(process.argv)).argv;
+
+async function openTab(url) {
+  result = await open(url);
+}
+
+function loadUrlsIntoArray(tabsFilePath) {
+  return new Promise((res, rej) => {
+    try {
+      const readInterface = readline.createInterface({
+        input: fs.createReadStream(tabsFilePath),
+        output: process.stdout,
+        console: false,
+      });
+
+      readInterface
+        .on("line", function (line) {
+          tabsArray.push(line);
+        })
+        .on("close", function () {
+          res();
+        });
+    } catch (err) {
+      console.log("error output: " + err);
+      rej(err);
+    }
+  });
+}
+
+async function openTabsDefaultBrowser(tabsFilePath = "./tabs.txt") {
+  const result = await loadUrlsIntoArray(tabsFilePath);
+  tabsArray.map((tab) => {
+    openTab(tab);
+  });
+}
+
+exports.openTabs = function () {
+  argv.filePath
+    ? openTabsDefaultBrowser(argv.filePath)
+    : openTabsDefaultBrowser();
+};
